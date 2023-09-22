5a249ea4613177a47fdcbf9d5fa7fd47003ce39e
Joakim Norborg <jocke4300@gmail.com>
2023-09-22 13:49:34 +0200

    Incoming

--- inl2.py
+++ inl2.py
@@ -1,34 +1,54 @@
-class Product:
-    def __init__(self, kod, namn, pris):
-        self.kod = kod
-        self.namn = namn
-        self.pris = pris
-class Rabatt:
-    def __init__(self, rabatt, datum):
-        self.rabatt = rabatt
-        self.datum = datum
+import os
+class ProduktManager:
+    def __init__(self):
+        pass
+    def ny_kund(self):
+        pass
+    def lägg_till_produk(self):
+        pass
+    def visa_varor(self):
+        pass
+class RabattManager:
+    def __init__(self):
+        pass
+    def ny_rabatt(self):
+        pass
+    def uppdatera_rabatt(self):
+        pass
+    def visa_rabatt(self):
+        pass
+class KvittoManager:
+    def __init__(self):
+        pass
+    def skapa_kvitto(self):
+        pass
+    def lägg_till_varor(self):
+        pass
+    def skriv_ut_kvitto(self):
+        pass
 
+def spara_data_till_fil(data, filnamn):
+    with open(filnamn, "w") as fil:
+        for key, items in data.items():
+            for item_key, item_value in items.items():
+                fil.write(f"{key}: {item_key}\n")
+                for attr, value in vars(item_value).items():
+                    fil.write(f"   {attr}: {value}\n")
+def ladda_data_från_fil(data, filnamn):
+    with open(filnamn, "r") as fil:
+        lines = fil.readlines()
+        key = None
+        for line in lines:
+            if line.startswith("produkter") or line.startswith("rabatter"
+                            ) or line.startswith("kvitton"):
+                key = line.strip().split(":")[0]
+            if key:
+                item_key, item_value = line.strip().split(": ")
+                data[key][item_key] = item_value
 
-
-
-produkter = {}
-
-rabatter = {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+produkter_manager = ProduktManager()
+rabatter_manager = RabattManager()
+kvitto_manager = KvittoManager()
 def main_meny():
     while True:
         print("Välkommen")
@@ -49,7 +69,19 @@ def main_meny():
         else:
             pass
 
-if __name__ == "__main::":
-    try:
-        with open("Produkter.txt", "r"):
+
+if __name__ == "__main__":
+    data = {'produkter': {},
+            'rabatter': {},
+            'kvitton': {},
+     }
+    spara_data_till_fil(data, "Produkter.txt")
+    spara_data_till_fil(data, "Rabatter.txt")
+    spara_data_till_fil(data, "Kvitton.txt")
+
+    ladda_data_från_fil(data, "Produkter.txt")
+    ladda_data_från_fil(data, "Rabatter.txt")
+    ladda_data_från_fil(data, "Kvitton.txt")
+    main_meny()
+    
             
